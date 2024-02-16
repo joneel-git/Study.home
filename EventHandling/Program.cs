6e80a09b364ba7c45075eb9a13a5fa3f86121967
joneel-git <johnj.es@outlook.com>
2024-02-16 12:27:54 +0100

    Started project EventHandling

--- EventHandling/Program.cs
+++ EventHandling/Program.cs
@@ -6,7 +6,6 @@
         {
             Car mycar = new Car("volvo", 4); //normal car
             E ecar = new E("tesla", 4); //electric car
-
             if (ecar is E)
             {
                 System.Console.WriteLine("our car is a: " + ecar.name);
