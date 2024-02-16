d8b7a84add71e252dc1f2bb1dd6240174fd167f6
joneel-git <johnj.es@outlook.com>
2024-02-16 12:10:07 +0100

    created inheritance parent class subclass

--- EventHandling/Program.cs
+++ EventHandling/Program.cs
@@ -4,7 +4,32 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car mycar = new Car("volvo", 4); //normal car
+            E ecar = new E("tesla", 4); //electric car
+
+            if (mycar is E)
+            {
+                System.Console.WriteLine();
+            }
         }
+    }
+}
+
+class Car
+{
+    public string name = ""; //fields
+    public int gasLevel = 0; //fields
+    public Car(string c, int g)
+    {
+        name = c;
+        gasLevel = g;
+    }
+}
+class E : Car
+{
+    public E(string c, int g) : base(c, g)
+    {
+        name = c;
+        gasLevel = g;
     }
 }
