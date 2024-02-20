75bacaa45c0823b5342680f63f106bc65ba64ceb
JoneeL-github <johnj.es@outlook.com>
2024-02-20 08:47:42 +0100

    added gas Simulation to the backend

--- EventHandling/Program.cs
+++ EventHandling/Program.cs
@@ -6,18 +6,26 @@
         {
             Car mycar = new Car("volvo", 4); //normal car
             E ecar = new E("tesla", 4); //electric car
-            if (mycar is E)
+            Bike bike = new Bike("Ninja", 3);
+
+            //to see different output change ecar to mycar;
+            if (ecar is E || bike is Bike)
             {
-                //true our car is an ECar
                 ecar.Drive();
                 Console.WriteLine("True our car is a " + ecar.name);
-
+                Console.WriteLine("");
+                bike.PutHelmetOn();
+                Console.WriteLine("the Bike is a " + bike.name);
             }
             else
             {
+                Console.WriteLine("");
                 mycar.Drive();
-                Console.WriteLine("False our car is a " + mycar.name);
+                Console.WriteLine(" our car is a " + mycar.name);
+
             }
+            mycar.GasSimulation();
+
             Console.ReadLine();
         }
     }
--- EventHandling/Vehicle.cs
+++ EventHandling/Vehicle.cs
@@ -11,9 +11,60 @@ namespace EventHandling
         }
         public virtual void Drive()
         {
-            Console.WriteLine("Motor_Engine goes Vroom.. ");
+            var color = Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Motor_Engine go VrOOooom.. ", color);
         }
+        //-----------------------------------
+        public int GasSimulation()
+        {
+            var color = Console.ForegroundColor; //default color
+            int gasLevel = this.gasLevel;
 
+            void Sleep()
+            {
+                Console.WriteLine("\n Sad to see you go..", color);
+                Thread.Sleep(1000);
+            }
+            bool choice = true;
+            do
+            {
+                Console.Write(" Do you want to continue? y/n (enter) ", color);
+                ConsoleKeyInfo info = Console.ReadKey();
+                if (info.Key == ConsoleKey.Enter)
+                {
+                    choice = true;
+                    continue;
+                }
+                else if (info.Key == ConsoleKey.N)
+                {
+                    Sleep();
+                    choice = false;
+                    break;
+                }
+                else if (info.Key == ConsoleKey.Y)
+                {
+                    if (gasLevel == 0)
+                    {
+                        Console.WriteLine("\n We are out pf Gas ..S " + gasLevel);
+                        choice = false;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n our Gas Level: " + gasLevel);
+                        gasLevel--;
+                        choice = true;
+                        continue;
+                    }
+                }
+                else
+                {
+                    choice = false;
+                }
+            } while (choice);
+            return gasLevel;
+            //-----------------------------------
+        }
     }
     public class E : Car
     {
@@ -25,7 +76,23 @@ namespace EventHandling
         }
         public override void Drive()
         {
-            Console.WriteLine("Electric_Engine go SssssHhhh  not Vroom.. ");
+            var color = Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Electric_Engine go SssssHhhh  not Vroom.. ", color);
+
+        }
+    }
+
+    public class Bike : Car
+    {
+        public Bike(string n, int g) : base(n, g)
+        {
+            name = n;
+            gasLevel = g;
+        }
+        public void PutHelmetOn()
+        {
+            var color = Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Helmet feels nice on the head.. ", color);
         }
     }
 }
