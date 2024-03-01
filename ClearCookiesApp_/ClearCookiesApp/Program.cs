243d589f332e75d34e047703dbd868f7ed4fc5a6
joneel-git <johnj.es@outlook.com>
2024-03-01 11:03:39 +0100

    added future project

---
+++ ClearCookiesApp_/ClearCookiesApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using TopLevel Statements
+using System.Diagnostics;
+// namespace ClearCookiesApp;
+// Console.WriteLine("Hello, World!");
+try
+{
+    string edgePath = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
+    Console.WriteLine("Attempting to clear Edge cookies.. ");
+    Thread.Sleep(1500);
+    Process.Start(edgePath);
+    Console.WriteLine("Edge cookies cleared successfully..! ");
+    Thread.Sleep(2000);
+
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error clearing Edge cookies: {e.Message}");
+    Console.ReadLine();
+}
